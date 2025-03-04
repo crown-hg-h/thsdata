b94eb76e7a4f4bb637c9b62a53162f90cd3b6bf3
bense <bense@gmail.com>
2025-03-04 20:19:25 +0800

    readthedoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import thsdata
+
 extensions = [
     'sphinx.ext.autodoc',
 ]
